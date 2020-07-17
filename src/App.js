e3c826414e1d40f0faf84e1ffe9125a15e1d991a
unknown <potlar1@hawkmail.newpaltz.edu>
2020-07-16 21:37:23 -0400

    nav bar filename changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { DetailsForm } from "./components/detailsForm";
+import { NavBar } from "./components/navBar";
 import "jquery/dist/jquery.min.js";
 import "popper.js/dist/popper.js";
 import "bootstrap/dist/js/bootstrap.min.js";
@@ -8,7 +8,7 @@ import { FormContent } from "./components/formContent";
 function App() {
   return (
     <div>
-      <DetailsForm />
+      <NavBar />
       <FormContent />
     </div>
   );
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 
-export const DetailsForm = () => {
+export const NavBar = () => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
